e90909859a59cd71340645a197f9df142397238e
Anaida Oksuzyan <aoa2190@gmail.com>
2024-04-09 18:08:35 +0300

    First task second commit

--- src/main/java/myThread/Main.java
+++ src/main/java/myThread/Main.java
@@ -10,36 +10,75 @@ public class Main {
     public static int maxCount = 0;
     public static int maxFreq = 0;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        for (int i = 0; i < 1000; i++) {
+//        Thread thread = new Thread(() -> {
+//
+//
+//            while (!Thread.interrupted()) {
+//                synchronized (sizeToFreq) {
+//                    try {
+//                        sizeToFreq.wait();
+//                        for (Map.Entry<Integer, Integer> entry : sizeToFreq.entrySet()) {
+//                            if (maxFreq < entry.getValue()) {
+//                                maxCount = entry.getKey();
+//                                maxFreq = entry.getValue();
+//                                break;
+//                            }
+//                        }
+//                        System.out.println(System.nanoTime() + ": Самое частое количество повторений на тек. момент " + maxCount + " (встретилось " + maxFreq + " раз)");
+//
+//                    } catch (InterruptedException e) {
+//                        throw new RuntimeException(e);
+//                    }
+//                }
+//            }
+//        });
+//
+//        thread.start();
+
+        for (int i = 0; i < 50; i++) {
             new Thread(() -> {
+
                 String genStr = generateRoute("RLRFR", 100);
 
-                long rCount = Arrays.stream(genStr.split("")).
-                        filter(ch -> "R".equals(ch)).count();
-                setSizeToFreq((int) rCount);
+                int rCount = (int) Arrays.stream(genStr.split("")).
+                        filter("R"::equals).count();
+                synchronized (sizeToFreq) {
+                    setSizeToFreq(rCount);
+                    //sizeToFreq.notify();
+                    try {
+                        Thread.sleep(300);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
             }).start();
         }
+        //thread.join();
+        //thread.interrupt();
+        synchronized (sizeToFreq) {
+            System.out.println("Самое частое количество повторений " + maxCount + " (встретилось " + maxFreq + " раз)");
+            System.out.println("Другие размеры:");
+            for (Map.Entry<Integer, Integer> freq : sizeToFreq.entrySet()) {
+                System.out.println("- " + freq.getKey() + " (" + freq.getValue() + " раз)");
 
-        System.out.println("Самое частое количество повторений " + maxCount + " (встретилось " + maxFreq + " раз)");
-        System.out.println("Другие размеры:");
-        for (Map.Entry<Integer, Integer> freq : sizeToFreq.entrySet()) {
-            System.out.println("- " + freq.getKey() + " (" + freq.getValue() + " раз)");
-
+            }
         }
     }
 
-    public static synchronized void setSizeToFreq(int count) {
-        if (sizeToFreq.containsKey(count)) {
-            Integer freq = sizeToFreq.get(count) + 1;
-            if (maxFreq < freq) {
-                maxFreq = freq;
-                maxCount = count;
+    public static void setSizeToFreq(int count) {
+        synchronized (sizeToFreq) {
+            if (sizeToFreq.containsKey(count)) {
+                Integer freq = sizeToFreq.get(count) + 1;
+                if (maxFreq < freq) {
+                    maxFreq = freq;
+                    maxCount = count;
+                }
+                sizeToFreq.put(count, freq);
+            } else {
+                sizeToFreq.put(count, 1);
             }
-            sizeToFreq.put(count, freq);
-        } else {
-            sizeToFreq.put(count, 1);
         }
     }
 
